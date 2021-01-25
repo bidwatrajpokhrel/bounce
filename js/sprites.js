90d7baed087b86fd3e8c4a7cdcece83462c0e787
Bidwat Raj Pokhrel <bidwatrajpokharel@gmail.com>
2021-01-25 18:33:23 +0545

    image path changed

--- js/sprites.js
+++ js/sprites.js
@@ -2,7 +2,7 @@ import SpriteSheet from './spritesheet.js';
 import { loadImage } from './loaders.js';
 
 export function loadBallSprite() {
-    return loadImage('/images/sprite.png').then(image => {
+    return loadImage('images/sprite.png').then(image => {
         const sprite = new SpriteSheet(image, 12, 12);
         sprite.define('ball', 2, 0);
         return sprite;
@@ -10,7 +10,7 @@ export function loadBallSprite() {
 }
 
 export function loadBackgroundSprite() {
-    return loadImage('/images/sprite.png').then(image => {
+    return loadImage('images/sprite.png').then(image => {
         const sprite = new SpriteSheet(image, 12, 12);
         sprite.define('back', 0, 6);
         sprite.define('ground', 1, 0);
